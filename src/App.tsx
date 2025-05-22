3c3e30ca0e806a780417ad9f1e94dc12f8b9eb88
jaehan <kyobo.jaehan@gmail.com>
2025-05-22 17:24:01 +0900

    로딩, 에러 화면 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import classNames from 'classnames/bind'
 import styles from './App.module.scss'
+import FullScreenMessage from './components/shared/FullScreenMessage'
 
 const cx = classNames.bind(styles)
 function App() {
-  return <div className={cx('container')}>App</div>
+  const [wedding, setWedding] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    fetch('http://localhost:8888/wedding')
+      .then((res) => {
+        if (!res.ok) throw new Error('청첩장 정보를 불러오지 못했습니다.')
+        return res.json()
+      })
+      .then((data) => {
+        setWedding(data)
+        setLoading(false)
+      })
+      .catch((err) => {
+        setError(true)
+        console.log('에러발생', err)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+
+  if (loading) {
+    return <FullScreenMessage type="loading" />
+  }
+
+  if (error) {
+    return <FullScreenMessage type="error" />
+  }
+
+  return <div className={cx('container')}>{JSON.stringify(wedding)}</div>
 }
 
 export default App
